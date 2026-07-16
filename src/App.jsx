9bc2342953d67d4e8ffc625dce40684680e0149f
Mahmoud Mohamed <hmdyhm54@gmail.com>
2026-07-16 03:33:06 +0200

    Try to make the mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import PageHeader from "./PageHeader/PageHeader";
 import SideBar from "./SideBarFolder/SideBar";
 import CardsContainer from "./CardsContainer/CardsContainer";
 import InfoCards from "./InfoCardsFolder/InfoCards";
+
+import logo from "./assets/images/logo.svg";
+import menuIcon from "./assets/images/icon-menu.svg";
+import Divider from "./Divider/Divider";
 export default function App() {
   return (
     <main className="main">
+      <header class="mobile-navbar hidden">
+        <img src={logo} alt="logo" className="logo-mobile-view" />
+        <button class="menu-btn">
+          <span class="menu-icon"></span>
+        </button>
+      </header>
       <SideBar />
       <div className="main-content">
         <PageHeader name="Lucia" status="CONFIRMED" />
